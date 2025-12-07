91229ad4f6bd0d907914242dd96f58f69f9d11de
Andrei-Laurentiu Ciurtin <149907852+CiurtinAndrei@users.noreply.github.com>
2025-12-07 18:20:22 +0200

    hard match + exclude

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ except SQLAlchemyError as e:
 
 
 def getNeedData(id):
+    #LEGEND: need_id, company_id, salary, category_id, category(name), subcategory_id, subcategory(name), city_id, city(name) latitude, longitude, schedule_id, schedule_name
     query = """
-    SELECT n.id as need_id, ns.schedule_id, sch.value AS schedule_type, n.salary, c.id AS company_id , c.name AS company_name, ctg.id AS ctg_id, 
-    ctg.value AS category, s.id as sctg_id, s.value AS subcategory, 
-    cty.id AS city_id, cty.name AS city_name, cty.lat as latitude, cty.lng as longitude
+    SELECT n.id as need_id, c.id AS company_id, n.salary, ctg.id AS category_id, ctg.value AS category, 
+    s.id AS subcategory_id, s.value AS subcategory, cty.id AS city_id, cty.name AS city, cty.lat as latitude, cty.lng as longitude, sch.id as schedule_id, sch.value AS schedule_name
     FROM needs n
     INNER JOIN companies c ON (n.company_id = c.id)
     INNER JOIN categories ctg ON(n.category_id = ctg.id)
@@ -27,11 +27,58 @@ def getNeedData(id):
     WHERE n.active = 1 AND n.signed = 1 AND n.finalized_status IS NULL AND n.id = :id;
     """
     result = conn.execute(text(query), {"id": id})
+    return result
+
+
+def getCandidates(need):
+    need_id = need.need_id
+    company_id = need.company_id
+    category_id = need.category_id
+    subcategory_id = need.subcategory_id
+    city_id = need.city_id
+    if subcategory_id == 128 or subcategory_id == 129:
+        subcategory_id = None
+    #LEGEND: candidate_id, salary_preference, experience_id, experience, education_id, education, category_id, category, subcategory_id, subcategory, city_id, city
+    query = """
+    SELECT c.id AS candidate_id , c.desired_salary AS salary_preference, c.experience_id, exp.value AS experience, c.education_id, edu.value AS education,
+    cde.category_id AS category_id, ctg.value AS category, cde.subcategory_id AS subcategory_id, sctg.value AS subcategory, ccty.city_id, cty.name as city
+    FROM candidates c
+    INNER JOIN education edu ON edu.id = c.education_id
+    INNER JOIN experiences exp ON exp.id = c.experience_id
+    INNER JOIN candidate_domain_experiences cde ON cde.candidate_id = c.id
+    INNER JOIN candidate_city ccty ON c.id = ccty.candidate_id
+    INNER JOIN cities cty ON ccty.city_id = cty.id AND ccty.city_id = :cityId
+    INNER JOIN categories ctg ON cde.category_id = ctg.id
+    INNER JOIN subcategories sctg ON cde.subcategory_id = sctg.id 
+
+    WHERE    /*  EXCLUSIONS  */
+        c.current_employer IS NULL
+        AND NOT EXISTS ( 
+            SELECT 1
+            FROM candidate_blocked_companies blc
+            WHERE blc.candidate_id = c.id
+            AND blc.company_id = :companyId
+        )
+        AND NOT EXISTS (
+            SELECT 1
+            FROM processes prc
+            WHERE prc.candidate_id = c.id
+            AND prc.need_id = :needId
+        )
+        /*  INCLUSIONS  */
+        AND (cde.category_id = :categoryId AND cde.subcategory_id = :subcategoryId)
+
+    ORDER BY c.education_id DESC, c.desired_salary ASC, c.id;
+    """
+    result = conn.execute(text(query), {"needId": need_id, "companyId":company_id, "categoryId":category_id, "subcategoryId":subcategory_id, "cityId":city_id})
 
     return result
 
-res = getNeedData(2454)
+need = getNeedData(2454).all()
 
-rows = res.all()
+candidates = getCandidates(need[0]).all()
 
-print(rows[0].need_id)
+print(need[0])
+
+print(candidates)
+
